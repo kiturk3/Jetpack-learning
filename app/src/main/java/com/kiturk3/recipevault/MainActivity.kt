cc6f888e9c8a740a44c0fb5e1f25f85073f6b8ce
Krutik Khokhara <kiturk3@users.noreply.github.com>
2026-06-25 15:10:52 +0530

    Day 3

--- app/src/main/java/com/kiturk3/recipevault/MainActivity.kt
+++ app/src/main/java/com/kiturk3/recipevault/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -21,13 +19,11 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
-
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,8 +31,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
-
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -165,18 +159,7 @@ fun RecipeCard(
                     fontFamily = FontFamily.Cursive,
                     fontWeight = FontWeight.Bold
                 )
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = "$30",
-                        style = MaterialTheme.typography.bodySmall,
-                        fontFamily = FontFamily.Cursive,
-                        fontWeight = FontWeight.Bold
-                    )
-                    FavButton(isFav = isFav, onToggle = onFavToggle)
-                }
-
+                FavButton(isFav = isFav, onToggle = onFavToggle)
             }
             Text(
                 text = subtitle,
